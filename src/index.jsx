d866036055d6e786bd86482f78de359221a1c587
Justin Evans <jurevans@gmail.com>
2016-08-11 11:52:48 -0400

    UI tests

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
-  document.getElementById('app')
+	<Voting pair={pair} hasVoted="Trainspotting" />,
+	document.getElementById('app')
 );
+
